3cdbaabd1f935e572a61cba00588beaf9fcc4210
Sander Tamm <162053604+TheBighi@users.noreply.github.com>
2026-09-01 13:07:37 +0300

    Enhance element addition and deletion in Selenium script
    Updated element selection and added functionality to click the add button multiple times and delete elements.

--- ylesanne3.py
+++ ylesanne3.py
@@ -1,18 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 import time
 
 driver = webdriver.Chrome()
 
 driver.get("https://the-internet.herokuapp.com/add_remove_elements/")
-
 time.sleep(2)
 
-add_button = driver.find_element(By.TAG_NAME, "button")
+add_button = driver.find_element(By.XPATH, "//button[text()='Add Element']")
 
-add_button.click()
+for _ in range(5):
+    add_button.click()
+    time.sleep(0.5)
 
-input()
+delete_buttons = driver.find_elements(By.CLASS_NAME, "added-manually")
+
+for button in delete_buttons:
+    button.click()
+    time.sleep(0.5)
+
+input("Vajuta Enter brauseri sulgemiseks...")
 
 driver.quit()
